1c79bf7ef00aa7fe20a791fdc4692dfca98bade4
odangosan <odangosan333@gmail.com>
2015-05-01 14:51:08 +0900

    [change]change static site template

---
+++ app.js
@@ -0,0 +1,23 @@
+var connect = require('connect');
+
+var serveStatic = require('serve-static');
+
+var port = process.env.PORT || 3000;
+
+var app = connect();
+
+var basicAuth = require('basic-auth-connect');
+var username = process.env.BASIC_AUTH_USERNAME;
+var password = process.env.BASIC_AUTH_PASSWORD;
+
+if (username && password) {
+  app.use(basicAuth(username, password));
+}
+
+app.use(serveStatic('build/', {
+  'index': ['index.html']
+}));
+
+app.listen(port);
+
+console.log('Server starting on http://0.0.0.0:%s', port);
