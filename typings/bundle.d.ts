c61e3bc58990b922814e0b1a90d337102cb4eb39
odangosan <odangosan333@gmail.com>
2015-05-01 12:54:07 +0900

    [add]d.tsファイルを追加

---
+++ typings/bundle.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="storejs\storejs.d.ts" />
+/// <reference path="typescript\phaser.d.ts" />
+/// <reference path="typescript\pixi.d.ts" />
+/// <reference path="typescript\p2.d.ts" />
