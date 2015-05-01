897da0ebb4f2a46e14b10359e08a247f40e67687
odangosan <odangosan333@gmail.com>
2015-05-02 03:10:34 +0900

    [fix]typescriptで動くようにした

--- scripts/build.js
+++ scripts/build.js
@@ -7,13 +7,45 @@ var __extends = this.__extends || function (d, b) {
 /// <reference path="../../typings/bundle.d.ts"/>
 var character;
 (function (character) {
-    var Stachoo = (function () {
-        function Stachoo(game, x, y, key, frame) {
-            this.sprite.anchor.set(0.5);
+    var Stachoo = (function (_super) {
+        __extends(Stachoo, _super);
+        function Stachoo(game, x, y, main, key, frame) {
+            _super.call(this, game, x, y, key, frame);
+            this.gravity = 800;
+            this.isJumping = false;
+            this.isFallingDown = false;
+            this.mainState = main;
+            game.physics.enable(this, Phaser.Physics.ARCADE);
+            this.anchor.set(0.5);
+            this.body.gravity.y = this.gravity;
             this.lastPole = 1;
+            game.add.existing(this);
+            this.game.input.onDown.add(this.prepareToJump(this), this);
         }
+        Stachoo.prototype.prepareToJump = function (stachoo) {
+            return function () {
+                if (stachoo.body.velocity.y === 0) {
+                    stachoo.powerBar = stachoo.game.add.sprite(stachoo.x, stachoo.y - 50, "powerbar");
+                    stachoo.powerBar.width = 0;
+                    stachoo.powerTween = stachoo.game.add.tween(stachoo.powerBar).to({ width: 100 }, 1000, "Linear", true);
+                    stachoo.game.input.onDown.remove(stachoo.prepareToJump, stachoo);
+                    stachoo.game.input.onUp.add(stachoo.jump(stachoo), stachoo);
+                }
+            };
+        };
+        Stachoo.prototype.jump = function (stachoo) {
+            return function () {
+                stachoo.jumpPower = -stachoo.powerBar.width * 3 - 100;
+                stachoo.powerBar.destroy();
+                stachoo.game.tweens.removeAll();
+                stachoo.body.velocity.y = stachoo.jumpPower * 2;
+                stachoo.isJumping = true;
+                stachoo.powerTween.stop();
+                stachoo.game.input.onUp.remove(stachoo.jump, stachoo.game);
+            };
+        };
         return Stachoo;
-    })();
+    })(Phaser.Sprite);
     character.Stachoo = Stachoo;
     var Pole = (function (_super) {
         __extends(Pole, _super);
@@ -21,6 +53,10 @@ var character;
             _super.call(this, game, x, y, key, frame);
             this.stachoo = stachoo;
             this.mainState = mainState;
+            Phaser.Sprite.call(this, game, x, y, "pole");
+            game.physics.enable(this, Phaser.Physics.ARCADE);
+            this.body.immovable = true;
+            this.poleNumber = mainState.placedPoles;
         }
         Pole.prototype.update = function () {
             if (this.stachoo.isJumping && !this.stachoo.isFallingDown) {
@@ -48,6 +84,7 @@ var main;
         function MainState() {
             _super.apply(this, arguments);
             this.score = 0;
+            this.topScore = 0;
             this.minPoleGap = 100;
             this.maxPoleGap = 300;
         }
@@ -58,46 +95,33 @@ var main;
             this.game.load.image("powerbar", "images/powerbar.png");
         };
         MainState.prototype.create = function () {
+            this.game.stage.backgroundColor = "#87CEEB";
             this.placedPoles = 0;
             this.poleGroup = this.game.add.group();
-            this.scoreText = this.game.add.text(10, 10, "-", { font: "bold 16px Arial" });
+            this.score = 0;
+            this.topScore = localStorage.getItem("topJumpScore") == null ? 0 : localStorage.getItem("topFlappyScore");
+            this.scoreText = this.game.add.text(10, 10, "-", { font: "bold 16px 'Hiragino Kaku Gothic ProN'" });
             this.updateScore();
-            this.game.stage.backgroundColor = "#87CEEB";
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            this.stachoo.sprite = this.game.add.sprite(80, 0, "ninja");
-            this.game.physics.arcade.enable(this.stachoo);
-            this.game.input.onDown.add(this.prepareToJump, this);
+            this.stachoo = new character.Stachoo(this.game, 80, 0, this, "ninja");
             this.addPole(80);
         };
         MainState.prototype.update = function () {
-            this.game.physics.arcade.collide(this.stachoo, this.poleGroup, this.chechLanding);
+            this.game.physics.arcade.collide(this.stachoo, this.poleGroup, this.checkLanding);
+            if (this.stachoo.y > this.game.height) {
+                this.die();
+            }
         };
         MainState.prototype.updateScore = function () {
             this.scoreText.text = "Score: " + this.score + "\nBest: " + this.topScore;
-        };
-        MainState.prototype.prepareToJump = function () {
-            if (this.stachoo.sprite.body.velocity.y === 0) {
-                this.powerBar = this.game.add.sprite(this.stachoo.sprite.x, this.stachoo.sprite.y - 50, "powerbar");
-                this.powerBar.width = 0;
-                this.powerTween = this.game.add.tween(this.powerBar).to({ width: 100 }, 1000, "Linear", true);
-                this.game.input.onDown.remove(this.prepareToJump, this);
-                this.game.input.onUp.add(this.jump, this);
-            }
-        };
-        MainState.prototype.jump = function () {
-            this.stachoo.jumpPower = -this.powerBar.width * 3 - 100;
-            this.powerBar.destroy();
-            this.game.tweens.removeAll();
-            this.stachoo.sprite.body.velocity.y = this.stachoo.jumpPower * 2;
-            this.stachoo.isJumping = true;
-            this.powerTween.stop();
-            this.game.input.onUp.remove(this.jump, this);
         };
         MainState.prototype.addNewPoles = function () {
             var maxPoleX = 0;
             this.poleGroup.forEach(function (item) {
                 maxPoleX = Math.max(item.x, maxPoleX);
             }, null);
+            var nextPolePosition = maxPoleX + this.game.rnd.between(this.minPoleGap, this.maxPoleGap);
+            this.addPole(nextPolePosition);
         };
         MainState.prototype.addPole = function (poleX) {
             if (poleX < this.game.width * 2) {
@@ -114,29 +138,27 @@ var main;
             localStorage.setItem("topJumpScore", Math.max(this.topScore, this.score).toString());
             this.game.state.start("main");
         };
-        MainState.prototype.chechLanding = function (n, p) {
-            if (p.y >= n.sprite.y + n.sprite.height / 2) {
-                var border = n.sprite.x - p.x;
-                if (Math.abs(border) > 20) {
-                    n.sprite.body.velocity.x = border * 2;
-                    n.sprite.body.velocity.y = -200;
+        MainState.prototype.checkLanding = function (stachoo, pole) {
+            if (pole.y >= stachoo.y + stachoo.height / 2) {
+                var border = stachoo.x - pole.x;
+                if (Math.abs(border) > 30) {
+                    stachoo.body.velocity.x = border * 2;
+                    stachoo.body.velocity.y = -200;
                 }
-                var poleDiff = p.poleNumber - n.lastPole;
+                var poleDiff = pole.poleNumber - stachoo.lastPole;
                 if (poleDiff > 0) {
-                    this.score += Math.pow(2, poleDiff);
-                    this.updateScore();
-                    n.lastPole = p.poleNumber;
+                    stachoo.mainState.score += Math.pow(2, poleDiff);
+                    stachoo.mainState.updateScore();
+                    stachoo.lastPole = pole.poleNumber;
                 }
-                if (this.stachoo.isJumping) {
-                    this.stachoo.isJumping = false;
-                    this.game.input.onDown.add(this.prepareToJump, this);
+                if (stachoo.isJumping) {
+                    stachoo.isJumping = false;
+                    stachoo.game.input.onDown.add(stachoo.prepareToJump, this);
                 }
             }
             else {
-                this.stachoo.isFallingDown = true;
-                this.poleGroup.forEach(function (item) {
-                    item.body.velocity.x = 0;
-                }, null);
+                stachoo.isFallingDown = true;
+                pole.body.velocity.x = 0;
             }
         };
         return MainState;
@@ -166,7 +188,7 @@ var boot;
 /// <reference path="boot.ts" />
 var Application = (function () {
     function Application(width, height, targetId) {
-        this.game = new Phaser.Game(width, height, Phaser.AUTO, targetId, null, true);
+        this.game = new Phaser.Game(width, height, Phaser.AUTO, targetId, null, false);
         this.game.state.add("boot", boot.bootState, false);
         this.game.state.add("main", main.MainState, false);
         this.game.state.start("boot");
